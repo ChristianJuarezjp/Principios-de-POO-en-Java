6ba3e8ccb9113c0442c6ef0cd6a999851da5fe07
ChristianJuarezjp <chrisjuarezale@gmail.com>
2024-07-28 18:42:40 -0600

    Implementación del sistema de gestión académica con POO en Java

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Estudiante estudiante1 = new Estudiante(1, "Christian", "Juarez", "2006-7-7", Estudiante.Estado.GRADUADO);
+        Estudiante estudiante2 = new Estudiante(2, "Alejandro", "Perez", "2006-7-7", Estudiante.Estado.MATRICULADO);
+
+
+        Curso curso1 = new Curso(1, "Artes y cultura", "Periodo Rojo Pablo Picasso", 3);
+        Curso curso2 = new Curso(2, "Sociales", "Conflicto armado interno ", 2);
+
+
+        GestorAcademico gestor = new GestorAcademico();
+
+        try {
+
+            gestor.matricularEstudiante(estudiante1);
+            gestor.matricularEstudiante(estudiante2);
+            gestor.agregarCurso(curso1);
+            gestor.agregarCurso(curso2);
+
+
+            gestor.inscribirEstudianteCurso(estudiante1, curso1.getId());
+            gestor.inscribirEstudianteCurso(estudiante2, curso2.getId());
+            gestor.desinscribirEstudianteCurso(estudiante1.getId(), curso1.getId());
+
+            gestor.inscribirEstudianteCurso(estudiante1, curso1.getId());
+        } catch (EstudianteYaInscritoException | EstudianteNoInscritoEnCursoException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(estudiante1);
+        System.out.println(curso1);
+        System.out.println(estudiante2);
+        System.out.println(curso2);
+    }
+
+}
